0d86d214a414a1e1d1ba47632885e9596d34c2f4
tristanchausson <tristanchausson@me.com>
2017-12-13 15:05:37 +0100

    add packages

--- gulpfile.js
+++ gulpfile.js
@@ -18,16 +18,20 @@ gulp.task('sass', function() {
     .pipe(browserSync.stream());
 });
 
-gulp.task('uglify', function (cb) {
-  pump([
+gulp.task('uglify', function () {
         gulp.src('assets/js/app.js'),
         uglify(),
         gulp.dest('public/js/')
-    ],
-    cb
-  ).pipe(browserSync.stream());
+    .pipe(browserSync.stream());
 });
 
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
 
 gulp.task('serve', ['sass', 'uglify'], function() {
     browserSync.init({
@@ -39,5 +43,7 @@ gulp.task('serve', ['sass', 'uglify'], function() {
 		gulp.watch('assets/js/**/*.js', ['uglify']);
 		 gulp.watch("public/*.html").on('change', browserSync.reload);
 });
+
+
 
 gulp.task('default', ['serve']);
